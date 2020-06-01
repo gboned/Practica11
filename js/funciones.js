e97f05546a74c030868733628e8e7379a67201c5
gboned <gboned@cifpfbmoll.eu>
2020-06-01 16:16:36 +0200

    Crear función que solicite a usuario id de personaje

--- js/funciones.js
+++ js/funciones.js
@@ -18,3 +18,19 @@ const PERSONAJE = function (personaje) {
     // Devuelvo en la página un texto con información sobre el personaje de la id introducida.
     document.write(`Hola mi nombre es ${personaje.name}, mi especie es ${personaje.species}, soy originario de ${personaje.origin.name}${"<br></br>"}`)
 };
+
+// Creo una función con el parámetro id.
+function pedirPersonaje(id) {
+    // Creo un prompt que solicite al usuario el número de id.
+    var id = Number(prompt("Introduce el id del personaje que quieres ver:"));
+    // En caso de introducir un número menor que 1 o mayor que 591, se devuelve por pantalla una frase determinada.
+    if (id < 1 || id > 591) {
+        document.write(`Este id no corresponde a ningún personaje actual.`)
+    } else {
+    // Si el número es correcto, se reemplaza la palabra "id" de la URL de personajes, por el número introducido.
+    const URL = `${API_URL}${PERSONAJES_URL.replace("id", id)}`;
+    // Se obtiene la información del personaje indicado.
+    $.get(URL, OPCIONES, PERSONAJE);
+    
+    }
+}
