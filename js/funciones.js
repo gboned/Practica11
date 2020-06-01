3589891d1a4209f636357a2fa4cdbf675256f520
gboned <gboned@cifpfbmoll.eu>
2020-06-01 16:16:09 +0200

    Crear función que devolverá los datos del personaje

--- js/funciones.js
+++ js/funciones.js
@@ -8,3 +8,13 @@
 const API_URL = "https://rickandmortyapi.com/api/";
 const PERSONAJES_URL = "character/id";
 const OPCIONES = { crossDomain: true };
+
+// Creo una función para conseguir ciertos datos de los personajes.
+const PERSONAJE = function (personaje) {
+    // En caso de que el origen sea desconocido, que se muestre una frase determinada.
+    if (personaje.origin.name === "unknown") {
+        personaje.origin.name = "un lugar desconocido";
+    }
+    // Devuelvo en la página un texto con información sobre el personaje de la id introducida.
+    document.write(`Hola mi nombre es ${personaje.name}, mi especie es ${personaje.species}, soy originario de ${personaje.origin.name}${"<br></br>"}`)
+};
