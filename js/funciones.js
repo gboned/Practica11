5c2a5375f87af949fb3623ea4b355dc704b28bec
gboned <gboned@cifpfbmoll.eu>
2020-06-01 16:15:22 +0200

    Añadir variables con datos de las URL

--- js/funciones.js
+++ js/funciones.js
@@ -3,3 +3,8 @@
 /*      RICK AND MORTY      */  
 /*                          */
 /****************************/
+
+// Indico en unas variables constantes los detalles de la URL a la que necesito acceder.
+const API_URL = "https://rickandmortyapi.com/api/";
+const PERSONAJES_URL = "character/id";
+const OPCIONES = { crossDomain: true };
